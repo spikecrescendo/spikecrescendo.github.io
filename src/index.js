fda56ba9b66cecc7e5edafca4fb17e7f0b4154a0
rohanmishr <rouangang@gmail.com>
2024-01-10 23:08:34 -0500

    add mobile responsiveness

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let blueAlliance = {
+    teams: [],
+    score: 0,
+    coopertition: false,
+    rankingPoints: 0,
+    auto: {
+        leave: false,
+        notesAmp: 0,
+        notesSpeaker: 0
+    },
+    teleop: {
+        notesAmp: 0,
+        notesSpeaker: 0,
+        notesAmplified: 0
+    },
+    endgame: {
+        notesTrap: 0,
+        spotlightCenter: false,
+        spotlightLeft: false,
+        spotlightRight: false
+    }
+}
